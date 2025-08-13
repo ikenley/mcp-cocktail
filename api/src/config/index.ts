d02ddedf878a92c86fa9b359c131424633544866
Ian Kenley <ikenley6@gmail.com>
2025-08-13 18:52:06 -0400

    remove forked objects

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -15,19 +15,11 @@ export class ConfigOptions {
     region: string;
   };
   baseDomain: string | null;
-  bedrockAgent: {
-    agentId: string;
-    agentAliasId: string;
-  };
   cognito: {
     userPoolId: string;
     userPoolClientId: string;
     userPoolClientSecret: string;
   };
-  fromEmailAddress: string;
-  imageMetadataTableName: string;
-  imageS3BucketName: string;
-  jobQueueUrl: string;
   // db: {
   //   host: string;
   //   port: number;
@@ -39,7 +31,6 @@ export class ConfigOptions {
   logs: { level: string };
   nodeEnv: string;
   port: number;
-  stateFunctionArn: string;
 }
 
 /** Get ConfigOptions from env vars.
@@ -51,10 +42,10 @@ export const getConfigOptions = () => {
   const authorizedEmails = JSON.parse(authorizedEmailsJson) as string[];
 
   const config: ConfigOptions = {
-    api: { prefix: "/ai/api" },
+    api: { prefix: "/cocktail/api" },
     app: {
       env: process.env.APP_ENV as AppEnv,
-      name: process.env.APP_NAME || "ai-api",
+      name: process.env.APP_NAME || "cocktail-api",
       version: process.env.APP_VERSION!,
     },
     authorizedEmails: authorizedEmails,
@@ -62,18 +53,11 @@ export const getConfigOptions = () => {
       region: process.env.AWS_REGION!,
     },
     baseDomain: process.env.BASE_DOMAIN || null,
-    bedrockAgent: {
-      agentId: process.env.BEDROCK_AGENT_ID!,
-      agentAliasId: process.env.BEDROCK_AGENT_ALIAS_ID!,
-    },
     cognito: {
       userPoolId: process.env.COGNITO_USER_POOL_ID!,
       userPoolClientId: process.env.COGNITO_USER_POOL_CLIENT_ID!,
       userPoolClientSecret: process.env.COGNITO_USER_POOL_CLIENT_SECRET!,
     },
-    fromEmailAddress: process.env.FROM_EMAIL_ADDRESS!,
-    imageMetadataTableName: process.env.IMAGE_METADATA_TABLE_NAME!,
-    jobQueueUrl: process.env.JOB_QUEUE_URL!,
     // db: {
     //   host: process.env.DB_HOST!,
     //   port: parseInt(process.env.DB_PORT!),
@@ -85,8 +69,6 @@ export const getConfigOptions = () => {
     logs: { level: process.env.LOGS__LEVEL || "http" },
     nodeEnv: process.env.NODE_ENV!,
     port: parseInt(process.env.PORT || "8086", 10),
-    imageS3BucketName: process.env.IMAGE_S3_BUCKET_NAME!,
-    stateFunctionArn: process.env.STATE_FUNCTION_ARN!,
   };
 
   return config;
